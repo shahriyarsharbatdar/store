9eeb31ad4129255ee135aaa3b572ecbf3c0bbcd2
shahriyarsharbatdar <sh.sharbatdar96@gmail.com>
2023-07-05 19:29:02 +0330

    build abstract Goods class

---
+++ src/model/Goods.java
@@ -0,0 +1,39 @@
+package model;
+
+public abstract class Goods {
+    private final String prefixId;
+    protected int count;
+    protected String id;
+    private static int COUNTER;
+
+    public Goods(String prefixId, int count, String id) {
+        this.prefixId = prefixId;
+        this.count = count;
+        this.id = prefixId + "-" + COUNTER;
+    }
+
+    public String getPrefixId() {
+        return prefixId;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+
+    public static int getCOUNTER() {
+        return COUNTER;
+    }
+
+    public static void setCOUNTER(int COUNTER) {
+        Goods.COUNTER = COUNTER;
+    }
+
+    @Override
+    public String toString() {
+        return "Goods{" +
+                ", id='" + id + '\'' +
+                ", count=" + count +
+                '}';
+    }
+}
